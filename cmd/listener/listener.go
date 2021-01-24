9c954c8fd7a6aee272d79060ece64029f515b1ca
Satssuki <danielarya185@gmail.com>
2021-01-24 15:39:21 +0700

    apply all handler to listener

---
+++ cmd/listener/listener.go
@@ -0,0 +1,35 @@
+package listener
+
+import (
+	"binance/listener/interfaces"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/sacOO7/gowebsocket"
+)
+
+// RunListener for binance websocket
+func RunListener() {
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt)
+
+	client := gowebsocket.New(os.Getenv("WSURL"))
+	client.OnConnected = interfaces.HandleOpen
+	client.OnTextMessage = interfaces.HandleMessage
+	client.OnConnectError = interfaces.HandleError
+	client.OnDisconnected = interfaces.HandleClose
+	client.OnPingReceived = interfaces.HandlePing
+	client.OnPongReceived = interfaces.HandlePong
+	client.Connect()
+
+	for {
+		select {
+		case <-interrupt:
+			log.Println("interrupt")
+			client.Close()
+			return
+		}
+	}
+}
